6331b69ef5114f9bf94966e1c84534fa8a19b5c9
rcda <rcda.tech@gmail.com>
2025-05-27 10:46:09 -0500

    removed old files

--- src/helpers/validations.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export const validatePath = (filePath) => {
-    // Implement path validation logic if necessary
-    if (path.isAbsolute(filePath) && !filePath.includes('..')) {
-      return filePath;
-    }
-    throw new Error('Invalid file path');
-} 
