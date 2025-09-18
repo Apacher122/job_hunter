c5e94dce31512d3599a957dae80e5ea44e5915eb
Rey Aparece <rcda.tech@gmail.com>
2025-09-17 22:17:55 -0500

    Stripping Prisma away

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,21 +1,27 @@
+import { Kysely, PostgresDialect } from 'kysely';
 import { Pool } from 'pg';
-d
+import { DB } from './schemas/ordo-meritum.schemas';
+
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: 'host.docker.internal',
   database: process.env.DB_NAME || 'ResumeBuilder',
   password: process.env.DB_PASSWORD || 'password',
   port: parseInt(process.env.DB_PORT || '5432'),
-  // add other options as needed
 });
 
-pool.connect()
-  .then(client => {
-    console.log('Connected to the database successfully');
-    client.release();
-  })
-  .catch(error => {
-    console.error('Error connecting to the database:', error);
-  });
+export const db = new Kysely<DB>({
+  dialect: new PostgresDialect({
+    pool,
+  }),
+});
 
-export default pool;
+async function testConnection() {
+  try {
+    await db.selectFrom('candidates').selectAll().limit(1).execute();
+    console.log('Connected to the database successfully (via Kysely)');
+  } catch (error) {
+    console.error('Error connecting to the database via Kysely:', error);
+  }
+}
+
